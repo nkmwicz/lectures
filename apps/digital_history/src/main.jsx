2b61053da18bd6b43e5d4018a721883cbb22a032
Nathan Michalewicz <nathan@mwicz.com>
2023-07-29 17:08:21 -0400

    add BrowserRouter

--- apps/digital_history/src/main.jsx
+++ apps/digital_history/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import { RecoilRoot } from "recoil";
 import { HelmetProvide } from "react-helmet-async";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RecoilRoot>
       <HelmetProvide>
-        <App />
+        <BrowserRouter basename="/digital-history">
+          <App />
+        </BrowserRouter>
       </HelmetProvide>
     </RecoilRoot>
   </React.StrictMode>
