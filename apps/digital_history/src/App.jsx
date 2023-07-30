c353980fa9cc9f2b8aea2fda0986c0dd2c7eac90
Nathan Michalewicz <nathan@mwicz.com>
2023-07-30 18:33:42 -0400

    add arrow

--- apps/digital_history/src/App.jsx
+++ apps/digital_history/src/App.jsx
@@ -1,11 +1,19 @@
-import { Nav } from "ui";
+import { Arrow, Nav } from "ui";
 import { Router } from "./router";
 import { Pages } from "./components/Pages";
 import "./App.css";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { allSlidesState, slideIndexState } from "ui";
+import { useSlideParameters } from "ui";
 function App() {
+  const allSlides = useRecoilValue(allSlidesState);
+  const [slideIndex, setSlideIndex] = useRecoilState(slideIndexState);
+  useSlideParameters({ allSlides, slideIndex, setSlideIndex });
+
   return (
     <>
       <Nav pages={Pages} />
+      <Arrow />
       <Router />
     </>
   );
