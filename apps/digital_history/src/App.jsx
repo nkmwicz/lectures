356e97dc1949b9229c7f25024bb4aabaf43e3b1a
Nathan Michalewicz <nathan@mwicz.com>
2023-07-30 08:55:30 -0400

    add nav

--- apps/digital_history/src/App.jsx
+++ apps/digital_history/src/App.jsx
@@ -1,7 +1,11 @@
+import { Nav } from "ui";
 import { Router } from "./router";
+import { Pages } from "./components/Pages";
+import "./App.css";
 function App() {
   return (
     <>
+      <Nav pages={Pages} />
       <Router />
     </>
   );
