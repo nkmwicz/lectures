15fabeb63c7a18ff98cc0fc4a3a504492793243c
Nathan Michalewicz <nathan@mwicz.com>
2023-07-30 08:37:41 -0400

    fix helmetprovider

--- apps/digital_history/src/main.jsx
+++ apps/digital_history/src/main.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { RecoilRoot } from "recoil";
-import { HelmetProvide } from "react-helmet-async";
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RecoilRoot>
-      <HelmetProvide>
+      <HelmetProvider>
         <BrowserRouter basename="/digital-history">
           <App />
         </BrowserRouter>
-      </HelmetProvide>
+      </HelmetProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
