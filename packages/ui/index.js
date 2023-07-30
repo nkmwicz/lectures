dce9c2c99f4ab33c8e1042119bc32ba6bf559024
Nathan Michalewicz <nathan@mwicz.com>
2023-07-30 18:35:49 -0400

    add exports

--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,1 +1,12 @@
 export { PageTemplate } from "./components/PageTemplate";
+export { Nav } from "./components/Nav";
+export { Arrow } from "./components/Arrow";
+
+export { useSlideParameters } from "./hooks/useSlideParameters";
+export { use1900Map } from "./hooks/use1900Map";
+
+export {
+  allSlidesState,
+  slideIndexState,
+  slideState,
+} from "./components/globalState";
