643715e9db28db14eaade28deecbbf6c03027e67
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-08-01 18:44:14 -0400

    add exports

--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,9 +1,11 @@
 export { PageTemplate } from "./components/PageTemplate";
+export { PageTemplateLeaflet } from "./components/PageTemplateLeaflet";
 export { Nav } from "./components/Nav";
 export { Arrow } from "./components/Arrow";
 
-// Map Utils
+// Map Utils / hooks
 export { useSlideParameters } from "./hooks/useSlideParameters";
+export { useMapCenterOfFirstMap } from "./hooks/useMapCenterOfFirstMap";
 export { useMap1900, map1900State } from "./hooks/Map1900Utils";
 export { useMap1914, map1914State } from "./hooks/Map1914Utils";
 
