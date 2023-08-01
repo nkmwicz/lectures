94342c5d949c276da3e0a32549dce7b1e12ebba4
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-08-01 18:41:48 -0400

    fix components

--- apps/modern_europe/src/main.jsx
+++ apps/modern_europe/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { RecoilRoot } from "recoil";
+import { HelmetProvider } from "react-helmet-async";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RecoilRoot>
+      <HelmetProvider>
+        <BrowserRouter basename="/modern-europe">
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
