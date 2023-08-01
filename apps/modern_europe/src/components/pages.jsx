b78579d037bee1a2eaee94a0ffd206eacce4ca39
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-08-01 18:41:58 -0400

    initial commit

---
+++ apps/modern_europe/src/components/pages.jsx
@@ -0,0 +1,163 @@
+export const Pages = [
+  {
+    title: "Introduction",
+    path: "/",
+  },
+  {
+    title: "Renaissance.1",
+    path: "/renaissance1",
+  },
+  {
+    title: "Renaissance.2",
+    path: "/renaissance2",
+  },
+  {
+    title: "Exploration.1",
+    path: "/exploration1",
+  },
+  {
+    title: "Exploration.2",
+    path: "/exploration2",
+  },
+  {
+    title: "Discussion1",
+    path: "/discussion1",
+  },
+  {
+    title: "Reformation",
+    path: "/reformation",
+  },
+  {
+    title: "Rel. Wars",
+    path: "/relwars",
+  },
+  {
+    title: "Discussion2",
+    path: "/discussion2",
+  },
+  {
+    title: "Sci. Rev.",
+    path: "/scirev",
+  },
+  {
+    title: "Colonialism",
+    path: "/colonialism",
+  },
+  {
+    title: "Discussion3",
+    path: "/discussion3",
+  },
+  {
+    title: "Slave Trade",
+    path: "/slavetrade",
+  },
+  {
+    title: "Enlightenment",
+    path: "/enlightenment",
+  },
+  {
+    title: "Discussion4",
+    path: "/discussion4",
+  },
+  {
+    title: "French Revolution-1",
+    path: "/fr-rev1",
+  },
+  {
+    title: "French Revolution-2",
+    path: "/fr-rev2",
+  },
+  {
+    title: "Discussion5",
+    path: "/discussion5",
+  },
+  {
+    title: "Ind Rev.",
+    path: "/ind-rev",
+  },
+  {
+    title: "isms",
+    path: "/isms",
+  },
+  {
+    title: "Discussion6",
+    path: "/discussion6",
+  },
+  {
+    title: "19c Revs.",
+    path: "/19c-revs",
+  },
+  {
+    title: "Nationalism",
+    path: "/nationalism",
+  },
+  {
+    title: "Discussion7",
+    path: "/discussion7",
+  },
+  {
+    title: "Imperialism-1",
+    path: "/imperialism1",
+  },
+  {
+    title: "Imperialism-2",
+    path: "/imperialism2",
+  },
+  {
+    title: "Discussion8",
+    path: "/discussion8",
+  },
+  {
+    title: "WWI",
+    path: "/wwi",
+  },
+
+  {
+    title: "WWI-Consequences",
+    path: "/wwi-consequences",
+  },
+  {
+    title: "Discussion9",
+    path: "/discussion9",
+  },
+  {
+    title: "Interwar-1",
+    path: "/interwar1",
+  },
+  {
+    title: "WWII",
+    path: "/wwii",
+  },
+  {
+    title: "Discussion10",
+    path: "/discussion10",
+  },
+  {
+    title: "Holocaust",
+    path: "/holocaust",
+  },
+  {
+    title: "Cold War-1",
+    path: "/coldwar1",
+  },
+  {
+    title: "Cold War-2",
+    path: "/coldwar2",
+  },
+  {
+    title: "Discussion11",
+    path: "/discussion11",
+  },
+  {
+    title: "Decolonization-1",
+    path: "/decolonization1",
+  },
+  {
+    title: "Decolonization-2",
+    path: "/decolonization2",
+  },
+  {
+    title: "Discussion12",
+    path: "/discussion12",
+  },
+];
