5a640bda5205a9cdaf9297467c1dfb5fdcdd8f74
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-08-07 16:27:27 -0400

    add useSlideParameters

--- apps/modern_europe/src/App.jsx
+++ apps/modern_europe/src/App.jsx
@@ -1,8 +1,9 @@
-import { Arrow, Nav } from "ui";
+import { Arrow, Nav, useSlideParameters } from "ui";
 import { Router } from "./router";
 import { Pages } from "./components/pages";
 
 function App() {
+  useSlideParameters();
   return (
     <>
       <Arrow />
